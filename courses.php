0dcc8a9a0bd23f7c1afeb2d96d389cddc5069626
Henry Ho <henrychho89@gmail.com>
2018-01-04 15:30:02 -0800

    Modified courses.php

--- courses.php
+++ courses.php
@@ -4,13 +4,8 @@
 $courseListFile = "./Courses/term1courses.txt";
 
 //split the courses into an array
-<<<<<<< HEAD
 $courseArray = file($courseListFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-=======
-// $test = file($courseListFile);
-$courseArray = file($courseListFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-// $courseArray = array(explode(", ", $test[0]));
->>>>>>> f760def114136b9fb49540ed0f5c90cd2ecc1232
+
 //count how many courses are in the file
 $counter = 0;
 for ($i = 0; $i < count($courseArray); $i++) {
